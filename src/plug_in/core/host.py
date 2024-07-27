b9706ad420a7c8641c4a2f1cd3fe62fb1fc26e88
kuta.0 <igor.kantorski@gmail.com>
2024-07-27 23:30:18 +0200

    fix: Improved type-checking on protocols that should have read-only attributes.

--- src/plug_in/core/host.py
+++ src/plug_in/core/host.py
@@ -5,8 +5,16 @@ from plug_in.proto.core_host import CoreHostProtocol
 
 @dataclass(frozen=True)
 class CoreHost[Subject](CoreHostProtocol[Subject]):
-    subject: type[Subject]
-    mark: Hashable = None
+    _subject: type[Subject]
+    _mark: Hashable = None
+
+    @property
+    def subject(self) -> type[Subject]:
+        return self._subject
+
+    @property
+    def mark(self) -> Hashable:
+        return self._mark
 
     def __hash__(self) -> int:
         return hash((self.subject, self.mark))
