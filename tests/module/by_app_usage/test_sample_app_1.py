0b3da2390bf4e568f44a83052961a66fe8fbd03c
kuta.0 <igor.kantorski@gmail.com>
2024-09-08 18:40:37 +0200

    test: Tested sample application

--- tests/module/by_app_usage/test_sample_app_1.py
+++ tests/module/by_app_usage/test_sample_app_1.py
@@ -1,5 +1,59 @@
 import importlib
+import sys
+
+from logging import getLogger
+from pathlib import Path
+from pytest import fixture
 
 
-def test_sample_app_1_runs():
-    importlib.import_module("tests.module.by_app_usage.sample_app_1")
+logger = getLogger(__name__)
+
+
+@fixture(scope="module")
+def sample_app_1_package_path():
+    path = str(
+        Path(__file__)
+        .parent.parent.parent.parent.joinpath("sample")
+        .joinpath("sample_app_1")
+        .absolute()
+    )
+
+    sys.path.insert(0, path)
+    logger.info(
+        "Inserted path:\n\t" "--> %s\n\t" "-- At position 0 of the sys.path", path
+    )
+
+    yield
+
+    try:
+        idx = sys.path.index(path)
+    except ValueError:
+        logger.warning(
+            "Previously inserted path:\n\t"
+            "--> %s\n\t"
+            "-- Not found in sys.path; Skipping cleanup.",
+            path,
+        )
+    else:
+        sys.path.pop(idx)
+        logger.info(
+            "Removing previously inserted path:\n\t"
+            "--> %s\n\t"
+            "-- It fas found at position: %s",
+            path,
+            idx,
+        )
+
+
+def test_sample_app_1_runs_and_injects(sample_app_1_package_path):
+
+    config = importlib.import_module("config")
+    config.configure()
+
+    api = importlib.import_module("api")
+    assert api.get_user("user1").id == "user1"
+    assert api.get_user_session_data("user1") == "some_data"
+
+    # Destroy import
+    del api
+    del config
